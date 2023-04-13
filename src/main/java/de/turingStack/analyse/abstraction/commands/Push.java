1937f2db482e886cd0ce1d6171daa453d07d6134
Tom <tom.loeser@fh-erfurt.de>
2023-04-13 08:36:05 +0200

    feat: add push command

--- src/main/java/de/turingStack/analyse/abstraction/commands/Push.java
+++ src/main/java/de/turingStack/analyse/abstraction/commands/Push.java
@@ -2,20 +2,51 @@ package de.turingStack.analyse.abstraction.commands;
 
 import de.turingStack.analyse.abstraction.pasing.Command;
 import de.turingStack.analyse.abstraction.pasing.CommandLine;
+import de.turingStack.analyse.abstraction.scanner.Token;
 import de.turingStack.analyse.abstraction.scanner.TokenCategory;
 
+import de.turingStack.stack.RegisterProvider;
+import de.turingStack.stack.objects.Register;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.Stack;
 
 public class Push extends Command {
-    public Push() {
-        super("push", Arrays.asList(
-                Arrays.asList(TokenCategory.KEYWORD, TokenCategory.NAME, TokenCategory.NAME, TokenCategory.LINEBREAK),
-                Arrays.asList(TokenCategory.KEYWORD, TokenCategory.NAME, TokenCategory.NUMBER, TokenCategory.LINEBREAK))
-        );
-    }
 
-    @Override
-    public void execute(CommandLine commandLine) {
+  public Push() {
+    super("push", Arrays.asList(
+        Arrays.asList(TokenCategory.KEYWORD, TokenCategory.NAME, TokenCategory.NAME,
+            TokenCategory.LINEBREAK),
+        Arrays.asList(TokenCategory.KEYWORD, TokenCategory.NAME, TokenCategory.NUMBER,
+            TokenCategory.LINEBREAK))
+    );
+  }
 
-    }
+  @Override
+  public void execute(CommandLine commandLine) {
+    commandLine.getFirstOf(TokenCategory.KEYWORD).ifPresent(token -> {
+      if (!token.content().equals(this.getName())) {
+        return;
+      }
+      Optional<Register> registerOptional = commandLine
+          .getFirstOf(TokenCategory.NAME)
+          .stream()
+          .map(Token::content)
+          .map(RegisterProvider::getRegister)
+          .map(register -> register.orElse(null))
+          .findFirst();
+
+      if (commandLine.tokens().stream().map(Token::category).toList().contains(TokenCategory.NUMBER)) {
+        commandLine.getFirstOf(TokenCategory.NUMBER)
+            .ifPresent(value -> registerOptional
+                .ifPresent(
+                    register -> register.push(value.content())
+                )
+            );
+      } else {
+        Token value = commandLine.tokens().get(2);
+        registerOptional.ifPresent(register -> register.push(value.content()));
+      }
+    });
+  }
 }
